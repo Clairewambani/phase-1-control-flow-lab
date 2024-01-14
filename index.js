9e3cfb652e036c7d9da0527ef8e46b616bf47867
Your Name <you@example.com>
2024-01-14 16:03:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (feet > 400 ) {
+      return 'I will gladly take your thirty bucks.';
+  }// Write your code here!
+}
+
+function ternaryCheckCity(city){
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  } // Write your code here!
 }
